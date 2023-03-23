e445217736dfa6e232224d265283c996cef654f1
GOMGOMHE <102653475+GOMGOMHE@users.noreply.github.com>
2023-03-23 19:44:55 +0900

    플레이어 이동 방패 공격 포션 히트 모션 구현

--- Assets/Script/성윤/Player.cs
+++ Assets/Script/성윤/Player.cs
@@ -6,8 +6,6 @@ using UnityEngine.InputSystem;
 
 public class Player : MonoBehaviour
 {
-    Vector3 inputDir = Vector3.zero;
-
     // 플레이어 이동 속도
     public float moveSpeed = 5.0f;
 
@@ -18,18 +16,26 @@ public class Player : MonoBehaviour
     public float jumpPower = 6.0f;
     bool IsJumping = false;
 
-    // 플레이어 방패
-    public GameObject target;
+    // 플레이어 방패 활성화/비활성화 변수
     private bool state;
 
     // 입력처리용 인풋액션
     PlayerInputActions inputActions;
+
+    // 플레이어 입력 방향
+    Vector3 inputDir = Vector3.zero;
 
     // 플레이어 리지드바디
     private Rigidbody rigid;
 
     // 애니메이터 컨트롤러
     Animator anim;
+
+    // 플레이어 애니메이션 끝나는 시간
+    float exitTime = 0.9f;
+
+    
+
 
     private void Awake()
     {
@@ -101,9 +107,9 @@ public class Player : MonoBehaviour
         {
             rigid.AddForce(Vector3.up * jumpPower, ForceMode.Impulse);  // 월드의 Up방향으로 힘을 즉시 가하기
             IsJumping = true;   // 점프중이라고 표시
-            anim.SetTrigger("IsJumping");
         }
     }
+
     // 착지했을 때 처리 함수
     private void OnGround()
     {
@@ -117,39 +123,86 @@ public class Player : MonoBehaviour
         {
             OnGround();     // 착지 함수 실행
         }
+        else if(collision.gameObject.CompareTag("Enemy"))
+        {
+            anim.SetTrigger("IsHit");
+        }
     }
 
     // 플레이어 공격 관련 이벤트 함수
     private void PlayerAttack(InputAction.CallbackContext context)
     {
         anim.SetTrigger("IsAttack"); //playerAnimator의 isAttack 트리거를 작동한다
+        StartCoroutine(AttackAnimationState());
+    }
+
+    // 공격애니메이션 코루틴
+    IEnumerator AttackAnimationState()
+    {
+        while (!anim.GetCurrentAnimatorStateInfo(0)
+        .IsName("Attack"))
+        {
+            // 전환 중일 때 실행되는 부분
+            yield return null;
+        }
+        while (anim.GetCurrentAnimatorStateInfo(0)
+        .normalizedTime < exitTime)
+        {
+            // 애니메이션 재생 중 실행되는 부분
+            inputActions.Player.Disable();  // 플레이어 입력 키 비활성화
+            yield return null;
+        }
+        // 애니메이션 완료 후 실행되는 부분
+        inputActions.Player.Enable();       // 플레이어 입력 키 활성화
     }
 
     // 플레이어 실드 관련 이벤트 함수
     private void PlayerShield(InputAction.CallbackContext context)
     {
-        //anim.SetBool("IsSheild", !context.canceled);
         Shield();
     }
 
-    //실드 나오게하기/안나오게하기
+    // 실드 애니메이션 활성화/비활성화
     void Shield()
     {
-        if (state == true)
+        if (state == false)
         {
-            target.SetActive(false);
-            state = false;
+            anim.SetBool("IsSheild", true);
+            state = true;
+            inputActions.Player.Move.Disable();
         }
         else
         {
-            target.SetActive(true);
-            state = true;
+            anim.SetBool("IsSheild", false);
+            state = false;
+            inputActions.Player.Move.Enable();
         }
     }
 
     // 플레이어 포션 관련 이벤트 함수
     private void PlayerPotion(InputAction.CallbackContext context)
     {
+        anim.SetTrigger("IsPotion");
+        StartCoroutine(PotionAnimationState());
+    }
 
+    // 포션애니메이션 코루틴
+    IEnumerator PotionAnimationState()
+    {
+        while (!anim.GetCurrentAnimatorStateInfo(0)
+        .IsName("Potion"))
+        {
+            // 전환 중일 때 실행되는 부분
+            yield return null;
+        }
+        while (anim.GetCurrentAnimatorStateInfo(0)
+        .normalizedTime < exitTime)
+        {
+            // 애니메이션 재생 중 실행되는 부분
+            inputActions.Player.Disable();      // 플레이어 입력 키 비활성화
+            yield return null;
+        }
+        // 애니메이션 완료 후 실행되는 부분
+        inputActions.Player.Enable();           // 플레이어 입력 키 활성화
     }
 }
